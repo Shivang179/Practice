cd6326cb5874eda6538ac563c3b1e81b417f915a
Shivang Shukla <shivangshukla179@gmail.com>
2026-07-30 16:48:09 +0530

    Time: 0 ms (100%), Space: 8.2 MB (95.83%) - LeetHub

--- 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
+++ 3014-minimum-number-of-pushes-to-type-word-i/3014-minimum-number-of-pushes-to-type-word-i.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
     int minimumPushes(string word) {
-        int n=word.size();
-        int ans=0;
-        if(n<=8) ans=n;
+        int n = word.size();
+        int ans;
+        if (n <= 8)
+            ans = n;
 
-        if(n>8 &&n<=16)
-         ans= (n-8)*2+8;
-    
-    if(n>16 && n<=24)
-    ans= 24+(n-16)*3;
-    
-    if(n>24 && n<=26)
-    ans=48+(n-24)*4;
-    return ans; 
+        if (n > 8 && n <= 16)
+            ans = (n - 8) * 2 + 8;
+
+        if (n > 16 && n <= 24)
+            ans = 24 + (n - 16) * 3;
+
+        if (n > 24 && n <= 26)
+            ans = 48 + (n - 24) * 4;
+        return ans;
     }
 };
